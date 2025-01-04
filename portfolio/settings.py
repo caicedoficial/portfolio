8874a2a8e73d7bbec48425ca7fd75cd851b9ba71
Alexander <caicedoficial@gmail.com>
2025-01-04 11:51:21 -0500

    Eliminar archivos y configuraciones innecesarias para simplificar el proyecto Django

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,7 +10,6 @@ DEBUG = True
 ALLOWED_HOSTS = [ '.vercel.app', 'localhost', 'alexandercaicedo.pro', 'www.alexandercaicedo.pro', '127.0.0.1' ]
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -23,7 +22,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -49,13 +47,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "portfolio.wsgi.app"
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
